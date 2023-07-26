118e408cc3e625d462866d3339cbc6d460c819b2
kreby455 <clebinhoalves2011@gmail.com>
2023-07-26 06:17:46 -0300

    Estudo Variaveis

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+/*
+Variáveis
+     
+   - let -> Pode alterar o valor quando quiser
+    -const -> VAlor Não pode ser declarado NOVAMENTE
+   - var -> DESCONTINUADO / NÃO USAR
+
+
+*/
+
+const abacate = 58
+
+console.log(abacate)
